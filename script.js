46fbd363d4dbb244a00c59a46917b7b9423c3ffb
sanacheriyoth797-arch <sanacheriyoth797@gmail.com>
2025-10-16 15:08:59 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    document.getElementById('result').value += num;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculate() {
+    let result = document.getElementById('result').value;
+    try {
+        document.getElementById('result').value = eval(result);
+    } catch (e) {
+        document.getElementById('result').value = 'Error';
+    }
+}
